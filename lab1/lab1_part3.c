f517012693f65ec4a060c282160383996541bc47
Andrew Kennedy <andrewk36@gmail.com>
2018-02-02 00:36:00 -0800

    blinking LED lab works now

--- lab1/lab1_part3.c
+++ lab1/lab1_part3.c
@@ -1,12 +1,12 @@
 #include "globals.h"
 #include <stdbool.h>
 #include <stdint.h>
-#include <string.h>
 #include <util/delay.h>
 
+__volatile__ uint8_t delayMultiplier = 1;
+__volatile__ uint8_t delayCountdown, inputByte;
+
 int main(int argc, char const* argv[]) {
-  volatile uint8_t delayMultiplier = 1;
-  volatile uint8_t delayCountdown, inputByte;
 
   serial_init();
 
@@ -14,67 +14,72 @@ int main(int argc, char const* argv[]) {
     delayCountdown = delayMultiplier;
     led_off();
     while (delayCountdown--) {
+      if ((inputByte = read_byte()) != 255) {
+        handleInput();
+      }
       _delay_ms(50);
     }
     led_on();
     delayCountdown = delayMultiplier;
     while (delayCountdown--) {
-      _delay_ms(50);
-    }
-    inputByte = read_byte();
-    if (inputByte != 255) {
-      switch (inputByte) {
-      case 'z':
-        if (delayMultiplier < UINT8_MAX) {
-          delayMultiplier++;
-        }
-        break;
-      case 'a':
-        if (delayMultiplier > 1) {
-          delayMultiplier--;
-        }
-        break;
-      default:
-        break;
+      if ((inputByte = read_byte()) != 255) {
+        handleInput();
       }
+      _delay_ms(50);
     }
   }
 }
 
+void handleInput() {
+  switch (inputByte) {
+  case 'z':
+    if (delayMultiplier < UINT8_MAX) {
+      delayMultiplier++;
+    }
+    break;
+  case 'a':
+    if (delayMultiplier > 1) {
+      delayMultiplier--;
+    }
+    break;
+  default:
+    break;
+  }
+}
 
 void led_on() {
-   //Set data direction to OUTPUT
-   //Clear Z high byte
-   __asm__ volatile ("clr r31");
-   //Set Z low byte to DDRB
-   __asm__ volatile ("ldi r30,0x24");
-   //Set 0x20 (DDRB bit 5 to 1)
-   __asm__ volatile ("ldi r18,0x20");
-   //Write 0x20 to location 0x24 (set LED pin as output)
-   __asm__ volatile ("st Z, r18");
+  // Set data direction to OUTPUT
+  // Clear Z high byte
+  __asm__ __volatile__("clr r31");
+  // Set Z low byte to DDRB
+  __asm__ __volatile__("ldi r30, 0x24");
+  // Set 0x20 (DDRB bit 5 to 1)
+  __asm__ __volatile__("ldi r18, 0x80");
+  // Write 0x20 to location 0x24 (set LED pin as output)
+  __asm__ __volatile__("st Z, r18");
 
-   //Set LED to ON
-   //Set Z low byte to LED register
-   __asm__ volatile ("ldi r30,0x25");
+  // Set LED to ON
+  // Set Z low byte to LED register
+  __asm__ __volatile__("ldi r30,0x25");
 
-   __asm__ volatile ("ldi r18,0x20");
-   __asm__ volatile ("st Z, r18");
+  __asm__ __volatile__("ldi r18,0x80");
+  __asm__ __volatile__("st Z, r18");
 }
 
 void led_off() {
-   //Set data direction to OUTPUT
-   //Clear Z high byte
-   __asm__ volatile ("clr r31");
-   //Set Z low byte to DDRB
-   __asm__ volatile ("ldi r30,0x24");
-   //Set 0x20 (DDRB bit 5 to 1)
-   __asm__ volatile ("ldi r18,0x20");
-   //Write 0x20 to location 0x24 (set LED pin as output)
-   __asm__ volatile ("st Z, r18");
+  // Set data direction to OUTPUT
+  // Clear Z high byte
+  __asm__ __volatile__("clr r31");
+  // Set Z low byte to DDRB
+  __asm__ __volatile__("ldi r30, 0x24");
+  // Set 0x20 (DDRB bit 5 to 1)
+  __asm__ __volatile__("ldi r18, 0x80");
+  // Write 0x20 to location 0x24 (set LED pin as output)
+  __asm__ __volatile__("st Z, r18");
 
-   //Set LED to OFF
-   __asm__ volatile ("ldi r30,0x25");
+  // Set LED to OFF
+  __asm__ __volatile__("ldi r30, 0x25");
 
-   __asm__ volatile ("ldi r18,0x00");
-   __asm__ volatile ("st Z, r18");
+  __asm__ __volatile__("ldi r18, 0x00");
+  __asm__ __volatile__("st Z, r18");
 }
